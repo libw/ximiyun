9c43f0f869abfd9d5788b7e95606b25349252bf8
unknown <unknown@example.com>
2018-10-18 14:19:56 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,20 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {    
+    test: [{
+        "url": "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1539846473120&di=eea422cb14f8781d7634b402958bebf7&imgtype=0&src=http%3A%2F%2Fc.hiphotos.baidu.com%2Fimage%2Fpic%2Fitem%2F77c6a7efce1b9d162f210013fedeb48f8d5464da.jpg",
+         "title": "这是标题一" ,
+         'classname':'bgr'
+      },         
+      {
+        "url": "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1539846473120&di=86317dfa7680b872e0662f28861e7f14&imgtype=0&src=http%3A%2F%2Fe.hiphotos.baidu.com%2Fimage%2Fpic%2Fitem%2Fb8014a90f603738d952a8450be1bb051f819ec64.jpg",
+        "title": "这是标题二" ,
+        'classname': 'bgb'       
+      }
+    ]
+  }
+
+})
